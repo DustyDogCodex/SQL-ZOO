faca3fc5b7444f967b0a8e6240d9050731856a2d
Varun Malaviya <varunmalaviya7@gmail.com>
2022-06-25 20:19:19 -0500

    initial commit for section 7 of SQLZoo.

---
+++ 7-more-JOIN.sql
@@ -0,0 +1,5 @@
+/* This tutorial introduces the notion of a join. The database consists of three tables movie , actor and casting .
+movie id 	title 	yr 	director 	budget 	gross
+actor id 	name
+casting movieid 	actorid 	ord */
+
